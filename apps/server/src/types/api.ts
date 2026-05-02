bcfa7e9d9167b80c9adb0cf605d44d5b35ba16ce
Rajkumar Paramesh <rajkumar_p@solartistech.local>
2026-05-02 17:23:37 +0530

    feat(api): implement standard response structure types

--- apps/server/src/types/api.ts
+++ apps/server/src/types/api.ts
@@ -1,12 +1,25 @@
 import { ErrorCode } from "@/constants/errorCode.js"
 
-export type ApiErrorResponse={
-  success:false,
-  code:ErrorCode,
-  message:string
+export type ApiSuccessResponse<T> = {
+  success: true;
+  data: T;
+};
+
+type Meta={
+  total:number;
+  page: number;
+  limit: number;
+  pages:number;
+}
+export type ApiPaginatedResponse<T>={
+  success: true;
+  data: T;
+  meta: Meta;
 }
 
-export type ApiSuccessResponse<T> ={
-  success:true,
-  data:T
-}
+export type ApiErrorResponse = {
+  success: false;
+  code: ErrorCode;
+  message: string;
+  stack?: string;
+};
